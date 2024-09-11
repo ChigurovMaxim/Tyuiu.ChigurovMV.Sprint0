045c1ec96306bd000661112fbfd030ea5c8aa751
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-09-11 22:48:12 +0500

    Task1

--- Tyuiu.ChigurovMV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.ChigurovMV.Sprint0.Task1.V0/Program.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.ChigurovMV.Sprint0.Task1.V0
         {
             Console.WriteLine("Hello, World!");
             string name = "Игорь";
-            int age = 45;
+            int age = 35;
             Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет!");
             Console.ReadKey();
         }
